57f87970f251f24ec614c3e2f617f2088e7386bf
Dhruv Karandikar <dhruvkarandikar@KTSPL-M-107.local>
2024-02-04 17:00:48 +0530

    used library

--- url_shortner.py
+++ url_shortner.py
@@ -1,1 +1,11 @@
-import pyshortners
+import pyshorteners
+
+
+'''Get API in progress for url shortners'''
+
+
+url = input('Enter url: ')
+
+
+print("Short url: ", pyshorteners.Shortener().tinyurl.short(url))
+
